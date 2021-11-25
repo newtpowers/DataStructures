c1b14d3a7a01ed1eca820d30dc988ea43da537b9
cpowers2021 <54912918+newtpowers@users.noreply.github.com>
2021-11-25 18:41:02 -0500

    Feat: Create a list of all odd numbers between 1 and a max number

--- arrayExercise.py
+++ arrayExercise.py
@@ -4,7 +4,7 @@ expenses = [2200, 2350, 2600, 2130, 2190]
 difference = expenses[1] - expenses[0]
 print(difference)
 # 2. Find out your total expense in first quarter (first three months) of the year.
-#for i in expenses:
+# for i in expenses:
 #    sum += expenses[i]
 print(sum(expenses, 0))
 # 3. Find out if you spent exactly 2000 dollars in any month
@@ -18,7 +18,7 @@ expenses.append(1980)
 # based on this
 expenses[3] = expenses[3] - 200
 
-heros=['spider man','thor','hulk','iron man','captain america']
+heros = ['spider man', 'thor', 'hulk', 'iron man', 'captain america']
 print(len(heros))
 heros.append('black panther')
 print(heros)
@@ -31,3 +31,13 @@ print(heros)
 heros.sort()
 print(heros)
 
+
+# Create a list of all odd numbers between 1 and a max number. Max number is something you need to take from a user
+# using input() function
+num = input()
+oddNums = []
+for x in range(1, num, 2):
+    oddNums.append(x)
+if num % 2 == 1:
+    oddNums.append(num)
+print(oddNums)
